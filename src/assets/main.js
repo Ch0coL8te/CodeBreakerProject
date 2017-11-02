babc1e94e830c939e4a29abe9cc38581b41e096a
Stanislav Burov <ch0col8te@me.com>
2017-11-03 00:21:08 +0300

    Changed addResult function

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,17 +4,17 @@ let attempt = document.getElementById('attempt');
 function guess() {
   let input = document.getElementById('user-guess');
     //add functionality to guess function here
-  
+
   if (answer.value == '' || attempt.value == '') {
-      setHiddenFields();  
+      setHiddenFields();
   }
-  
+
   if (!validateInput(input.value)) {
     return false;
   } else {
     attempt.value++;
   }
-  
+
 }
 
 //implement new functions here
@@ -22,16 +22,21 @@ function getResults(input) {
   let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
   for (i = 0; i < input.length; i++) {
     if (input.charAt(i) == answer.value.charAt(i)) {
-      html += '';
+      html += '<span class="glyphicon glyphicon-ok"';
+    } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+      html += '<span class="glyphicon glyphicon-transfer"></span>';
+    } else {
+      html += '<span class="glyphicon glyphicon-remove"></span>';
     }
   }
-  
+  html += '</div></div>';
+  document.getElementById('results').innerHTML += html;
 }
 
 function setHiddenFields() {
       answer.value = Math.floor(Math.random() * 9999).toString(), // Округляем до целого числа и преобразуем в строку
       attempt.value = 0;
-  
+
   while (answer.value.length < 4) { // Если answer не 4-х значное число - дабавляем 0 перед ним, пока не станет 4х значным
     answer = "0" + answer;
   }
